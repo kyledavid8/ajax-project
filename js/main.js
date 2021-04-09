b72d4b7b26640d48a2165ef17c5e62dabd4a788d
Kyle Cunningham <kyle8david@gmail.com>
2021-04-08 22:38:50 -0600

    user can search by date #1

--- js/main.js
+++ js/main.js
@@ -10,14 +10,12 @@ var resultPage = document.querySelector('.result-page');
 
 var findButton = document.querySelector('.home-search');
 var submit = document.querySelector('.submit');
-var data = document.querySelector('.data');
+var dataOutline = document.querySelector('.data-outline');
 var back = document.querySelector('.back');
 
 var searchBox = document.querySelector('#search-box');
 
 var backHome = ['search-page'];
-
-var renderCounter = 0;
 
 var main = document.querySelector('main');
 
@@ -37,10 +35,8 @@ Finder.prototype.changePage = function (event) {
     searchPage.className = 'search-page';
     holiday.status = 'search-page';
   } else if (event.target === submit) {
-    if (renderCounter === 0) {
-      holiday.renderResults();
-      renderCounter++;
-    }
+    dataOutline.innerHTML = '';
+    holiday.renderResults();
     resultPage.className = 'result-page';
     holiday.status = 'result-page';
   } else if (event.target === back) {
@@ -62,16 +58,50 @@ Finder.prototype.renderResults = function () {
   xhr.open('GET', 'https://holidayapi.com/v1/holidays?pretty&key=c1e47cbf-5b67-44b0-8359-4fe2afc3ae3a&country=US&year=2020');
   xhr.responseType = 'json';
   xhr.addEventListener('load', function () {
-    var loc = 0;
-    while (xhr.response.holidays.length > loc) {
-      if (searchBox.value === xhr.response.holidays[loc].date) {
-        break;
+    var i = 0;
+    var addy = [];
+    while (xhr.response.holidays.length > i) {
+      if (searchBox.value === xhr.response.holidays[i].date) {
+        addy.push(i);
+        i++;
       } else {
-        loc++;
+        i++;
       }
     }
 
-    if (loc === 149) {
+    if (addy.length !== 0) {
+      for (var loc = 0; addy.length > loc; loc++) {
+        var data = document.createElement('div');
+        data.className = 'data';
+        dataOutline.appendChild(data);
+
+        var p = document.createElement('p');
+        p.textContent = xhr.response.holidays[addy[loc]].name;
+        data.appendChild(p);
+
+        p = document.createElement('p');
+        p.textContent = xhr.response.holidays[addy[loc]].date;
+        data.appendChild(p);
+
+        p = document.createElement('p');
+        p.textContent = xhr.response.holidays[addy[loc]].weekday.date.name;
+        data.appendChild(p);
+
+        p = document.createElement('p');
+        var pub = '';
+        if (xhr.response.holidays[addy[loc]].public !== true) {
+          pub = 'No';
+        } else {
+          pub = 'Yes';
+        }
+        p.textContent = 'Public? ' + pub;
+        data.appendChild(p);
+      }
+    } else {
+      data = document.createElement('div');
+      data.className = 'data';
+      dataOutline.appendChild(data);
+
       var par = document.createElement('p');
       par.textContent = 'This holiday is unavailable';
       data.appendChild(par);
@@ -79,28 +109,6 @@ Finder.prototype.renderResults = function () {
       par = document.createElement('p');
       par.textContent = 'Please try a different date';
       data.appendChild(par);
-    } else {
-      var p = document.createElement('p');
-      p.textContent = xhr.response.holidays[loc].name;
-      data.appendChild(p);
-
-      p = document.createElement('p');
-      p.textContent = xhr.response.holidays[loc].date;
-      data.appendChild(p);
-
-      p = document.createElement('p');
-      p.textContent = xhr.response.holidays[loc].weekday.date.name;
-      data.appendChild(p);
-
-      p = document.createElement('p');
-      var pub = '';
-      if (xhr.response.holidays[loc].public !== true) {
-        pub = 'No';
-      } else {
-        pub = 'Yes';
-      }
-      p.textContent = 'Public? ' + pub;
-      data.appendChild(p);
     }
   });
   xhr.send();
